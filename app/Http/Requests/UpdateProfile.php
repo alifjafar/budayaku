eb00e59028980c530196251325373c7f4689cadd
Alif Jafar <alifjafar@gmail.com>
2018-09-11 23:39:18 +0700

    Change Password Done !

---
+++ app/Http/Requests/UpdateProfile.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProfile extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'  => 'required',
+            'gender' => 'required',
+            'telp'  => 'required|digits_between:10,12',
+            'address' => 'required',
+        ];
+    }
+}
