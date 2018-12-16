955b0b1ac816cfb132fd382b72740eaf53691931
Alif Jafar <alifjafar@gmail.com>
2018-12-16 11:47:33 +0700

    Booking Redirect to Booking List

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -62,7 +62,7 @@ class BookingController extends Controller
 
         Notification::send($user, new BookingNotification(Auth::user()->profile->name));
 
-        return redirect(route('homepage'));
+        return redirect()->route('booking-list');
 
     }
 }
